21967a7d174c15055653dcc647b438709277ee6e
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-08-14 17:55:44 +0900

    住所を出せるようにした

--- LocationAndDirection/ViewController.swift
+++ LocationAndDirection/ViewController.swift
@@ -29,7 +29,7 @@ class ViewController: UIViewController ,CLLocationManagerDelegate{
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+        purposeSetting()
         getLocation()
     }
 
@@ -71,8 +71,50 @@ class ViewController: UIViewController ,CLLocationManagerDelegate{
             return
         }
         
+        let latitude = resultLocation.latitude
+        let longtitude = resultLocation.longitude
+        
+//        let latitude = 35.967622
+//        let longtitude = 139.755292
+        
+        reverseGeocode(latitude: latitude,longitude: longtitude)
+        
+        hereLat.text = latitude.description
+        hereLong.text = longtitude.description
+        
         print(resultLocation)
         
+    }
+    
+    //緯度経度から住所を決定し、現在地のlabelに貼り付ける
+    private func reverseGeocode(latitude:CLLocationDegrees, longitude:CLLocationDegrees) {
+        let location = CLLocation(latitude: latitude, longitude: longitude)
+        let geocoder = CLGeocoder()
+        geocoder.reverseGeocodeLocation(location) { (response, error) in
+            let res = response?.first
+//            print("res?.administrativeArea")
+//            print(res?.administrativeArea ?? "取れてません")
+//            print("res?.locality")
+//            print(res?.locality ?? "取れてません")
+//            print("res?.subLocality")
+//            print(res?.subLocality ?? "取れてません")
+//            print((res?.administrativeArea)! + (res?.locality)! + (res?.subLocality)!)
+            
+            let label = (res?.administrativeArea)! + (res?.locality)! + (res?.subLocality)!
+            self.hereLabel.text = label
+        }
+    }
+    
+    //目的地のlabelや住所を手動入力
+    private func purposeSetting(){
+        let address = "埼玉県春日部市大沼"
+        self.purposeLabel.text = address
+        let geocorder = CLGeocoder()
+        geocorder.geocodeAddressString(address) { (response, error) in
+            let res = response?.first
+            self.purposLat.text = res?.location?.coordinate.latitude.description
+            self.purposLong.text = res?.location?.coordinate.longitude.description
+        }
     }
 }
 

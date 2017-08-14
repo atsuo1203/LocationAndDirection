a7b787cb435d7e83d64fff2de0319476b3481922
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-08-14 14:53:09 +0900

    各viewをswiftファイルに関連付けた

--- LocationAndDirection/ViewController.swift
+++ LocationAndDirection/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hereLocationView: HereLocationView!
+    @IBOutlet weak var purposeLocationView: PurposeLocationView!
+    @IBOutlet weak var compassView: CompassView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.

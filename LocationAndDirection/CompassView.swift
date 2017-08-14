061de6ad08c291079350b615c0aa6996e32b7dd9
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-08-14 14:49:46 +0900

    .swift作成

---
+++ LocationAndDirection/CompassView.swift
@@ -0,0 +1,21 @@
+//
+//  CompassView.swift
+//  LocationAndDirection
+//
+//  Created by Atsuo Yonehara on 2017/08/14.
+//  Copyright © 2017年 Atsuo Yonehara. All rights reserved.
+//
+
+import UIKit
+
+class CompassView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

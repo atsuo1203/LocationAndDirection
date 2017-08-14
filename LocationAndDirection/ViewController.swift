bd7cfc4f2ce99d8ce337c85b65325d43ea2bde68
atsuo1203 <atsuo@cps.im.dendai.ac.jp>
2017-08-14 15:31:28 +0900

    現在地取得完了

--- LocationAndDirection/ViewController.swift
+++ LocationAndDirection/ViewController.swift
@@ -7,15 +7,24 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,CLLocationManagerDelegate{
 
     @IBOutlet weak var hereLocationView: HereLocationView!
     @IBOutlet weak var purposeLocationView: PurposeLocationView!
     @IBOutlet weak var compassView: CompassView!
+    
+    //現在地座標
+    var hereLocation: CLLocationCoordinate2D?
+    var locationManager:CLLocationManager!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        getLocation()
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +32,41 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    //現在地を取る処理
+    func getLocation(){
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    //現在地取得の際のManager
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            break
+        case .authorizedAlways, .authorizedWhenInUse:
+            break
+        }
+    }
+    
+    //現在地取得の際のManager 毎秒連続して位置情報を取得している
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let newLocation = locations.last else {
+            return
+        }
+        
+        hereLocation = newLocation.coordinate
+        guard let resultLocation = hereLocation else {
+            print("位置情報取れてません")
+            return
+        }
+        
+        print(resultLocation)
+        
+    }
 }
 
